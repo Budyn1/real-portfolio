6a8078af954e26a34ed92a30c4f8db7b154d46bd
Kacper <kacperbudyka@gmail.com>
2022-06-23 19:51:50 +0200

    update

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./skillsCSS.css"
 import Skill from "./skill.js"
-import { FaHtml5, FaCss3, FaSass, FaReact, FaGithub } from "react-icons/fa"
-import { SiReactrouter, SiJavascript } from "react-icons/si"
+import { FaHtml5, FaCss3, FaSass, FaReact, FaGithub, FaGitAlt, FaBootstrap } from "react-icons/fa"
+import { SiReactrouter, SiJavascript, SiRedux, SiTailwindcss } from "react-icons/si"
 
 const Skills = (props) => (
   <div className="skillsPage" id="skill">
@@ -10,11 +10,15 @@ const Skills = (props) => (
     <div className="skillsCon">
       <Skill sk=<FaHtml5 color={"orange"} size={props.display > 760 ? 90 : 45} />/>
       <Skill sk= <FaCss3 color={"lightblue"} size={props.display > 760 ? 90 : 45} />/>
+      <Skill sk= <FaBootstrap color={"#8700FF"} size={props.display > 760 ? 90 : 45} />/>
       <Skill sk= <SiJavascript color={"yellow"} size={props.display > 760 ? 90 : 45} />/>
       <Skill sk= <FaReact color={"aqua"} size={props.display > 760 ? 90 : 45} />/>
+      <Skill sk= <SiRedux color={"purple"} size={props.display > 760 ? 90 : 45} />/>
       <Skill sk= <SiReactrouter color={"white"} size={props.display > 760 ? 90 : 45} />/>
+      <Skill sk= <SiTailwindcss color={"aqua"} size={props.display > 760 ? 90 : 45} />/>
       <Skill sk= <FaSass color={"pink"} size={props.display > 760 ? 90 : 45} />/>
       <Skill sk= <FaGithub color={"white"} size={props.display > 760 ? 90 : 45} />/>
+      <Skill sk= <FaGitAlt color={"red"} size={props.display > 760 ? 90 : 45} />/>
     </div>
   </div>
 );
